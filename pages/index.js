0c580809703bc48019d4162ac4b755a3bc0ef44f
nishi-oka <nishioka31415@gmail.com>
2024-10-26 22:59:27 +0900

    Update markdownblog

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import matter from 'gray-matter';
-import PostCard from '../components/postcard';
+import PostCard from '../components/PostCard';
 import { useState } from 'react';
 
 export const getStaticProps = () => {
